d1f3fe6a781fdce420b84eddb263c846d7fefee4
fabeto10 <f0414475@gmail.com>
2022-05-31 15:23:57 +0000

    terminado con cosas por cambiar

--- src/js/component/TrafficsLight.js
+++ src/js/component/TrafficsLight.js
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { array } from "prop-types";
 //include images into your bundle
 
 //create your first component
 export const TrafficsLight = () => {
-	const [selectColor, setSelectColor] = useState("");
+	const [selectClass, setSelectClass] = useState("red");
+
+	// const lights = document.querySelectorAll('.light')
+	// let counter = 0
+	// let arr = ["newRed","newYellow",'newGreen']
+	// const evento = () => {
+	// 	lights[counter].className = 'light'
+	// 	counter++
+	// 	if(counter > array.length) counter= 0;
+	// 	const lightNow = lights[counter]
+	// 	lightNow.classList.add(lightNow.getAttribute(arr))
+	// }
+	// setInterval(evento, 1000)
 
 	return (
 		<>
@@ -12,26 +25,25 @@ export const TrafficsLight = () => {
 			<div className="base">
 				<div
 					onClick={() => {
-						setSelectColor("red");
+						setSelectClass("red");
 					}}
-					className={
-						"light red" + (selectColor === "red" ? " glow" : "")
-					}></div>
+					className={`light ${
+						selectClass === "red" ? "newRed" : "red"
+					}`}></div>
 				<div
 					onClick={() => {
-						setSelectColor("yellow");
+						setSelectClass("yellow");
 					}}
-					className={
-						"light yellow" +
-						(selectColor === "yellow" ? " glow" : "")
-					}></div>
+					className={`light ${
+						selectClass === "yellow" ? "newYellow" : "yellow"
+					}`}></div>
 				<div
 					onClick={() => {
-						setSelectColor("green");
+						setSelectClass("green");
 					}}
-					className={
-						"light green" + (selectColor === "green" ? " glow" : "")
-					}></div>
+					className={`light ${
+						selectClass === "green" ? "newGreen" : "green"
+					}`}></div>
 			</div>
 		</>
 	);
